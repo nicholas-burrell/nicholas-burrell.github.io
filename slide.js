87c978be4a79e11b169ee622cd05300df8018b53
nicholas-burrell <45nickb@gmail.com>
2024-03-01 00:43:04 -0700

    damn

--- slide.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var paragraphs = document.querySelectorAll("#about .slide-in");
-
-    paragraphs.forEach(function(paragraph) {
-        paragraph.classList.add("active");
-    });
-});
